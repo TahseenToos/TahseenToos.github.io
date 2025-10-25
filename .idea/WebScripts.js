fdffc0e81b501820d7708233f47b82ca8ef09d17
Tahseen Khan <mtkhan1808@gmail.com>
2025-10-25 21:49:12 +0100

    Made stylesheet and script, building main page

---
+++ .idea/WebScripts.js
@@ -0,0 +1,15 @@
+﻿function LightDarkToggle(){
+    
+    var element = document.body;
+    const lightDarkToggler = document.getElementById('theme_changer');
+    
+    element.classList.toggle('dark_mode');
+    console.log(lightDarkToggler);
+    
+    if(element.classList.contains('dark_mode')){
+    lightDarkToggler.textContent = 'Light Mode';
+    }
+    else {
+    lightDarkToggler.textContent = 'Dark Mode';
+    }
+}
